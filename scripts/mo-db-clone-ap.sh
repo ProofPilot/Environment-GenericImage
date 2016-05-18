9c01c0cc7339604ea3e0ef3c56936ff191d050ef
Volodymyr Sheptytsky <vsheptytsky@gmail.com>
2016-05-18 22:20:39 +0300

    Moved to new repo!

---
+++ scripts/mo-db-clone-ap.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+S_NAME=$1 
+S_PASS=$2
+D_NAME=$3
+D_PASS=$4
+
+#SOURCE=$(aptible db:tunnel $S_NAME --port 12345) &
+#echo Source process ID: $SOURCE
+#sleep 10
+
+#DESTIN=$(aptible db:tunnel $D_NAME --port 12346) &
+#echo Destination process ID $DESTIN
+#sleep 10
+
+#$SOURCE=$(aptible db:tunnel $S_NAME --port 12345 &)
+#$DESTIN=$(aptible db:tunnel $D_NAME --port 12346 &)
+
+TMP_DIR=/tmp/`date +mongodb-backup-%Y.%m.%d_%H.%M.%S`
+echo $TMP_DIR
+
+
+aptible db:tunnel $S_NAME --port 12345 &
+SOURCE=$!
+echo Source process ID: $SOURCE
+sleep 30
+
+/usr/bin/mongodump \
+    --host 127.0.0.1 \
+    --port 12345 \
+    --username aptible \
+    --password $S_PASS \
+    --db db \
+    --out $TMP_DIR
+          
+aptible db:tunnel $D_NAME --port 12346 &
+DESTIN=$!
+echo Destination process ID $DESTIN
+sleep 30
+
+kill $SOURCE 
+
+/usr/bin/mongorestore \
+    --host 127.0.0.1 \
+    --port 12346 \
+    --ssl \
+    --sslAllowInvalidHostnames \
+    --sslAllowInvalidCertificates \
+    --username aptible \
+    --password $D_PASS \
+    --db db \
+    $TMP_DIR
+
+
+kill $DESTIN
+
+rm -rf $TMP_DIR
+
---
+++ scripts/my-db-clone-ap.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+S_NAME=$1 
+S_PASS=$2
+D_NAME=$3
+D_PASS=$4
+
+#SOURCE=$(aptible db:tunnel $S_NAME --port 12345) &
+#echo Source process ID: $SOURCE
+#sleep 10
+
+#DESTIN=$(aptible db:tunnel $D_NAME --port 12346) &
+#echo Destination process ID $DESTIN
+#sleep 10
+
+#$SOURCE=$(aptible db:tunnel $S_NAME --port 12345 &)
+#$DESTIN=$(aptible db:tunnel $D_NAME --port 12346 &)
+
+
+
+aptible db:tunnel $S_NAME --port 12345 &
+SOURCE=$!
+echo Source process ID: $SOURCE
+sleep 10
+aptible db:tunnel $D_NAME --port 12346 &
+DESTIN=$!
+echo Destination process ID $DESTIN
+sleep 10
+
+
+/usr/bin/mysqldump -v \
+    --host=127.0.0.1 \
+    --port=12345 \
+    --user=aptible-nossl \
+    --password=$S_PASS \
+    --quick --routines --add-locks --add-drop-table --events  db | \
+/usr/bin/mysql -v \
+    --host=127.0.0.1 \
+    --port=12346 \
+    --user=aptible-nossl \
+    --password=$D_PASS \
+    db
+
+
+kill $SOURCE $DESTIN
